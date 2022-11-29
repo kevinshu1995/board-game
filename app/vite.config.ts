0158c94e9c91fd1736620873741604c63f4c2468
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-11-29 11:58:55 +0800

    feat: 🎸 [app] devDependencies & vite server.proxy

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,11 +1,28 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import "dotenv/config";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    resolve: {
-        alias: [{ find: "@", replacement: "/src" }],
-    },
-    plugins: [vue()],
-});
+export default ({ mode }) => {
+    return defineConfig({
+        resolve: {
+            alias: [{ find: "@", replacement: "/src" }],
+        },
+        server: {
+            proxy: {
+                "/functions": {
+                    target: process.env.VITE_SUPABASE_FUNCTION_URL,
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/functions/, ""),
+                },
+                "/supabase": {
+                    target: process.env.VITE_SUPABASE_URL,
+                    changeOrigin: true,
+                    rewrite: path => path.replace(/^\/supabase/, ""),
+                },
+            },
+        },
+        plugins: [vue()],
+    });
+};
 
