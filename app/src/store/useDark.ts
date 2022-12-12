4d2cde4b84dac58a0737eb0f4d8892604d4605cc
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-12 11:22:42 +0800

    squash dark mode btn

--- app/src/store/useDark.ts
+++ app/src/store/useDark.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import { useDark as vueUseDark, useToggle } from "@vueuse/core";
 
-export const useDark = defineStore("useAuth", () => {
+export const useDark = defineStore("useDark", () => {
     const isDark = vueUseDark();
     const toggleDark = useToggle(isDark);
 
