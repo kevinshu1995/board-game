10601bf16d17b7d58a503e8104fff53182b7706f
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-19 17:53:00 +0800

    feat: 🎸 [app] BaseSelect

---
+++ app/src/components/widget/form/select/BaseSelect.vue
@@ -0,0 +1,46 @@
+<template>
+    <label
+        v-if="props.label"
+        :for="props.id"
+        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+    >
+        {{ props.label }}
+    </label>
+    <select
+        :id="props.id"
+        :value="props.modelValue"
+        @input="emitValue"
+        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    >
+        <option
+            v-for="option in props.options"
+            :value="option.value"
+            :disabled="option.disabled || false"
+        >
+            {{ option.text }}
+        </option>
+    </select>
+</template>
+
+<script setup lang="ts">
+type Option = {
+    text: string;
+    value: any;
+    disabled?: boolean;
+};
+
+const emit = defineEmits(["update:modelValue"]);
+
+const props = defineProps<{
+    label?: string;
+    options: Option[];
+    id: string;
+    modelValue: any;
+}>();
+
+function emitValue(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    emit("update:modelValue", target.value);
+}
+</script>
+
