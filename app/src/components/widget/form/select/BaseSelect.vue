5abe51499b40a56814dd4055040ddc38cb8b74ab
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-22 11:58:28 +0800

    feat: 🎸 [app] component - baseSelect wrap a div

--- app/src/components/widget/form/select/BaseSelect.vue
+++ app/src/components/widget/form/select/BaseSelect.vue
@@ -1,25 +1,27 @@
 <template>
-    <label
-        v-if="props.label"
-        :for="id"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-    >
-        {{ props.label }}
-    </label>
-    <select
-        :id="id"
-        :value="props.modelValue"
-        @input="emitValue"
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    >
-        <option
-            v-for="option in props.options"
-            :value="option.value"
-            :disabled="option.disabled || false"
+    <div>
+        <label
+            v-if="props.label"
+            :for="id"
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
         >
-            {{ option.text }}
-        </option>
-    </select>
+            {{ props.label }}
+        </label>
+        <select
+            :id="id"
+            :value="props.modelValue"
+            @input="emitValue"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        >
+            <option
+                v-for="option in props.options"
+                :value="option.value"
+                :disabled="option.disabled || false"
+            >
+                {{ option.text }}
+            </option>
+        </select>
+    </div>
 </template>
 
 <script setup lang="ts">
