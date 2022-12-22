c9515dede797f66afa43566900f698f0840a3ef4
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-22 12:28:51 +0800

    feat: 🎸 [app] view - Dashboard filter form

--- app/src/components/view/Dashboard/Dashboard.vue
+++ app/src/components/view/Dashboard/Dashboard.vue
@@ -1,14 +1,55 @@
 <template>
-    <div class="text-xl">dashboard</div>
-    <div>state: {{ !!publicRoomListState }}</div>
-    <div>isReady: {{ isReadyPublicRoomList }}</div>
-    <div>isLoading: {{ isLoadingPublicRoomList }}</div>
+    <div class="w-full">
+        <!-- <div class="text-xl">dashboard</div>
+        <div>state: {{ !!publicRoomListState }}</div>
+        <div>isReady: {{ isReadyPublicRoomList }}</div>
+        <div>isLoading: {{ isLoadingPublicRoomList }}</div> -->
+
+        <div class="space-y-4">
+            <!-- Query form -->
+            <div class="space-y-2">
+                <div class="flex gap-4 flex-wrap">
+                    <BaseSearchInput
+                        placeholder="Search room title"
+                        class="grow"
+                        :debounce="1000"
+                        v-model:value-model="publicRoomQuery.keyword"
+                    />
+
+                    <!-- <CollapsedForm :games="games" v-model:form="publicRoomQuery" /> -->
+                    <BaseDropdown custom-style="round" theme="light-n-border">
+                        <template #trigger>
+                            <div class="p-2">
+                                <Icon name="filter" class="w-6 h-6" />
+                            </div>
+                        </template>
+                        <template #dropdown>
+                            <div class="flex flex-col p-4 gap-4 w-60">
+                                <CollapsedForm :games="games" v-model:form="publicRoomQuery" />
+                            </div>
+                        </template>
+                    </BaseDropdown>
+                </div>
+                <p>Current query -></p>
+            </div>
+            <div>room cards</div>
+            <div>pagination</div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, toRaw } from "vue";
+import { onMounted, ref, reactive, toRaw, watch } from "vue";
+import { watchThrottled } from "@vueuse/core";
+
 import { getGames, getPublicRooms } from "@/api";
 import { useAsyncLocalState } from "@composable";
+import { RoomListBody } from "@/api/types";
+import BaseDropdown from "@widget/dropdown/BaseDropdown.vue";
+import BaseSearchInput from "@widget/form/searchInput/BaseSearchInput.vue";
+import CollapsedForm from "./components/CollapsedForm.vue";
+
+type PublicRoomQuery = Required<RoomListBody>;
 
 const {
     state: publicRoomListState,
@@ -21,7 +62,10 @@ const {
     apiPromise: handleGetPublicRooms,
 });
 
-const publicRoomQuery = reactive({
+const games = ref([]);
+
+const publicRoomQuery: PublicRoomQuery = reactive({
+    game_id: null,
     status: null, // RoomStatus.processing | RoomStatus.waiting | RoomStatus.complete,
     get_mine_rooms: false,
     is_full: null,
@@ -31,27 +75,61 @@ const publicRoomQuery = reactive({
     keyword: null,
 });
 
-onMounted(() => {
-    handleGetGames();
-    executePublicRoomListState();
-});
-
 async function handleGetGames() {
     const { data, error } = await getGames();
     console.log("games", data, error);
-    if (error === null) {
+    if (error !== null) {
         return [];
     }
+    games.value = data;
+    // use this for now until we have multiple games
+    publicRoomQuery.game_id = data[0].game_id;
     return data;
 }
 
+// 調整要送出去的 query
+function formatPublicRoomQuery(): PublicRoomQuery {
+    const rawPublicRoomQuery: PublicRoomQuery = toRaw(publicRoomQuery);
+    return Object.keys(rawPublicRoomQuery).reduce((acc: any, key) => {
+        const queryKey = key as keyof PublicRoomQuery;
+        let queryValue = rawPublicRoomQuery[queryKey];
+        if (queryKey === "game_id") {
+            // game_id 只接受 number
+            queryValue = queryValue !== null ? Number(queryValue) : null;
+        } else {
+            // 空字串改成 null
+            queryValue = queryValue === "" ? null : queryValue;
+        }
+        return {
+            ...acc,
+            [key]: queryValue,
+        };
+    }, {});
+}
+
 async function handleGetPublicRooms() {
-    const { data, error } = await getPublicRooms(toRaw(publicRoomQuery));
+    const { data, error } = await getPublicRooms(formatPublicRoomQuery());
     console.log("rooms", data, error);
     if (error !== null) {
         return [];
     }
     return data;
 }
+
+onMounted(async () => {
+    handleGetGames();
+    executePublicRoomListState();
+});
+
+watchThrottled(
+    publicRoomQuery,
+    () => {
+        executePublicRoomListState();
+    },
+    {
+        throttle: 1000,
+        immediate: false,
+    }
+);
 </script>
 
---
+++ app/src/components/view/Dashboard/components/CollapsedForm.vue
@@ -0,0 +1,96 @@
+<template>
+    <BaseSelect label="遊戲" :options="gamesOption" v-model:modelValue="formObj.game_id" />
+    <BaseSelect label="房間狀態" :options="roomStatusOptions" v-model:modelValue="formObj.status" />
+    <BaseSelect label="密碼" :options="passwordOptions" v-model:modelValue="formObj.has_password" />
+    <BaseSelect label="房間人數" :options="isFullOptions" v-model:modelValue="formObj.is_full" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { RoomListBody, RoomStatus, RoomStatusZhTw } from "@/api/types";
+import { convertEnum } from "@utils";
+
+import BaseSelect from "@widget/form/select/BaseSelect.vue";
+
+const props = defineProps<{
+    form: Pick<RoomListBody, "game_id" | "status" | "has_password" | "is_full">;
+    games: object[];
+}>();
+
+const emit = defineEmits(["update:form"]);
+
+const formObj = computed({
+    get() {
+        return props.form;
+    },
+    set(val) {
+        emit("update:form", val);
+    },
+});
+
+// room status
+const formatRoomStatus = (obj: { key: string; value: any }[]) =>
+    obj.filter(({ key }) => key !== "None");
+const roomStatusZhTw = formatRoomStatus(convertEnum(RoomStatusZhTw));
+const roomStatusOptions = formatRoomStatus(convertEnum(RoomStatus))
+    .map(item => {
+        return {
+            value: item.value,
+            text: roomStatusZhTw.find(zhTwItem => zhTwItem.key === item.key)?.value || "",
+        };
+    })
+    .concat([
+        {
+            value: "",
+            text: "全部",
+        },
+    ]);
+
+// game
+const gamesOption = computed(() => {
+    return props.games.map((item: any) => ({
+        text: item.name_zh_tw,
+        value: item.game_id,
+    }));
+    // not needed for now
+    // .concat([
+    //     {
+    //         text: "全部",
+    //         value: "",
+    //     },
+    // ]);
+});
+
+// has_password
+const passwordOptions = [
+    {
+        text: "不需密碼",
+        value: false,
+    },
+    {
+        text: "需要密碼",
+        value: true,
+    },
+    {
+        text: "全部",
+        value: "",
+    },
+];
+
+// is_full
+const isFullOptions = [
+    {
+        text: "人數已滿",
+        value: true,
+    },
+    {
+        text: "人數未滿",
+        value: false,
+    },
+    {
+        text: "全部",
+        value: "",
+    },
+];
+</script>
+
