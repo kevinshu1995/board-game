d46b44e4292218626dfda8722370206e82c1c79b
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-22 11:59:58 +0800

    fix: 🐛 [app] layout - make width full

--- app/src/components/layout/app/AppLayout.vue
+++ app/src/components/layout/app/AppLayout.vue
@@ -3,7 +3,7 @@
         <TheNavbar class="shadow-lg" />
         <div class="flex min-h-full grow">
             <TheSidebar class="shadow-2xl self-stretch" />
-            <div class="overflow-auto self-stretch p-8">
+            <div class="overflow-auto self-stretch p-8 w-full">
                 <RouterView />
             </div>
         </div>
