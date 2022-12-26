488f886477ff7546009474ed66441f5957b49500
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-26 15:19:08 +0800

    fix: 🐛 [app] init store (useAuth) on App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import AppLayout from "./components/layout/app/AppLayout.vue";
+import { useAuth } from "@/store";
+
+// all stores needed everywhere
+const auth = useAuth();
 </script>
 
 <template>
