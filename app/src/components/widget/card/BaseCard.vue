2ccb82da51b4de9c1d2947dd2beefcf5d2992522
wenshiu.hsu <wenshiu.hsu@gmail.com>
2022-12-26 15:20:15 +0800

    feat: 🎸 [app] new component - BaseCard

---
+++ app/src/components/widget/card/BaseCard.vue
@@ -0,0 +1,39 @@
+<template>
+    <component
+        v-bind="extraBindOnRootElement"
+        :is="props.tag"
+        :class="[
+            props.padding,
+            'block bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700',
+        ]"
+    >
+        <slot />
+    </component>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps({
+    tag: {
+        type: String,
+        default: "div",
+    },
+    padding: {
+        type: String,
+        default: "p-6",
+        validator(val: string) {
+            return /-?p[x|y]?-./.test(val);
+        },
+    },
+});
+
+const extraBindOnRootElement = computed(() => {
+    const attrs: { [attr: string]: any } = {};
+    if (props.tag === "a") {
+        attrs.href = "#";
+    }
+    return attrs;
+});
+</script>
+
